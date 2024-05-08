b4f0091dd9b1ca6477d66d4204168fe9dd9860a1
Rave JS <hswatter@mail.ru>
2024-05-09 02:29:54 +0300

    button and fix

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, FC } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
 import styles from './CountryDetails.module.css'
 import { Container, Row, Col } from 'react-bootstrap'
@@ -38,27 +38,32 @@ const CountryDetails: FC = () => {
   }
 
   return (
-    <Container className={styles.container}>
-      <h2>{countryDetails.name.common}</h2>
-      <Row>
-        <Col sm={6}>
-          <p>Capital: {countryDetails.capital}</p>
-          <p>Region: {countryDetails.region}</p>
-          <p>Population: {countryDetails.population}</p>
-          <p>Area: {countryDetails.area} km²</p>
-          <p>Language(s): {Object.values(countryDetails.languages).join(', ')}</p>
-        </Col>
-        <Col sm={6} className="d-flex justify-content-center align-items-center">
-          <div className="image-wrapper">
-            <img
-              src={countryDetails.flags.png}
-              alt={countryDetails.name.common}
-              className="img-fluid"
-            />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container className={styles.container}>
+        <h2>{countryDetails.name.common}</h2>
+        <Row>
+          <Col sm={6}>
+            <p>Capital: {countryDetails.capital}</p>
+            <p>Region: {countryDetails.region}</p>
+            <p>Population: {countryDetails.population}</p>
+            <p>Area: {countryDetails.area} km²</p>
+            <p>Language(s): {Object.values(countryDetails.languages).join(', ')}</p>
+          </Col>
+          <Col sm={6} className="d-flex justify-content-center align-items-center">
+            <div className="image-wrapper">
+              <img
+                src={countryDetails.flags.png}
+                alt={countryDetails.name.common}
+                className="img-fluid"
+              />
+            </div>
+          </Col>
+        </Row>
+        <Link to="/">
+          <button className={styles.button}>На главную</button>
+        </Link>
+      </Container>
+    </>
   )
 }
 
